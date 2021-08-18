1eb7452f10bdeef38e982267ce7944e3c117ec01
CalebWilson <63938606+CalebWilson@users.noreply.github.com>
2021-08-18 16:42:51 -0400

    deleted extraneous files

--- database/test.sql
+++
@@ -1,6 +0,0 @@
-			select BoothNum, CategoryName, ProjectNum, Title, Abstract
-			from Project, Booth, Category
-			where
-				Project.BoothID    = Booth.BoothID AND
-				Project.CategoryID = Category.CategoryID AND
-				ProjectID          = 1;
